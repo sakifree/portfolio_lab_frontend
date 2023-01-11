50e95dcc0a7df9555feb0599630aa8016cd1e584
Sakinah Freeman <freemansakinah@gmail.com>
2023-01-10 23:25:54 -0500

    sets up routes and components

--- src/router.js
+++ src/router.js
@@ -1,9 +1,14 @@
 import { createRoutesFromElements, createBrowserRouter, Route } from "react-router-dom";
 import App from "./App"
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
 
 const router = createBrowserRouter(createRoutesFromElements(
     <Route path="/" element={<App/>}>
-
+        <Route path="" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/projects" element={<Projects/>}/>
     </Route>
 ))
 
